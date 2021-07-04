1714eccf7820da650adbdf3a3bec45655c543831
Marc Nuri <marc@marcnuri.com>
2021-07-04 06:45:02 +0200

    🎉 Quarkus 2.0.0.Final project bootstrapped

---
+++ src/test/java/com/marcnuri/demo/kmp/quote/NativeQuoteResourceIT.java
@@ -0,0 +1,9 @@
+package com.marcnuri.demo.kmp.quote;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeQuoteResourceIT extends QuoteResourceTest {
+
+    // Execute the same tests but in native mode.
+}
